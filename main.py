c50e7179d997b9f30dccd09131d2dbbf9025bcd5
Siddhudhole <shidhudhole358@gmail.com>
2024-01-08 10:11:07 +0530

    add view task function

--- main.py
+++ main.py
@@ -5,4 +5,20 @@ tasks = []
 
 def add_task(description, priority=0):
     tasks.append({'description': description, 'priority': priority})
-    print(f'Task "{description}" added successfully.')
+    print(f'Task "{description}" added successfully.') 
+
+
+def view_tasks():
+    if tasks:
+        for i in tasks:
+            print(i['priority'],i['description'])
+    else :
+        print("Task is not Present")
+
+
+
+
+
+
+
+add_task()
